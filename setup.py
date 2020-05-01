161650ff1073cd6fd8cd271ef7266e7c95a80e88
vmoeykens <vincent@beta.team>
2020-05-01 14:16:30 -0400

    Adds nanogui back as package name

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
    description='Python bindings for the C++ GUI library nanogui.',
    author='Vincent Moeykens',
    author_email='vincent@beta.team',
-   packages=[''],
-   package_dir={'': 'bin'},
-   package_data={'': ['libnanogui.so', 'nanogui.cp37-win_amd64.pyd', 'nanogui.dll', 'nanogui.so']},
+   packages=['nanogui'],
+   package_dir={'nanogui': 'bin'},
+   package_data={'nanogui': ['libnanogui.so', 'nanogui.cp37-win_amd64.pyd', 'nanogui.dll', 'nanogui.so']},
 )
