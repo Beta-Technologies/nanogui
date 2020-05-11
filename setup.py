98c6fd08571c21ebf39a734279d772bf393aae1f
vmoeykens <vincent@beta.team>
2020-05-11 12:59:54 -0400

    Reverts nanogui package setup to remove "nanogui" namespace

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
    name='nanogui',
-   version='1.0',
+   version='1.0.0',
    description='Python bindings for the C++ GUI library nanogui.',
    author='Vincent Moeykens',
    author_email='vincent@beta.team',
-   packages=['nanogui',],
-   package_dir={'nanogui': 'bin',},
-   package_data={'nanogui': ['nanogui.cp37-win_amd64.pyd', 'nanogui.dll', 'nanogui.so', 'libnanogui.so'],},
+   packages=['',],
+   package_dir={'': 'bin',},
+   package_data={'': ['nanogui.cp37-win_amd64.pyd', 'nanogui.dll', 'nanogui.so', 'libnanogui.so'],},
 )
