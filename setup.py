622a9ba8fa6d2abca6f8d676745c794e1719c937
vmoeykens <vincent@beta.team>
2020-05-15 18:39:26 -0400

    Adds binaries and updates setup.py to pull both 3.6 and 3.7 binaries

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email='vincent@beta.team',
     packages=['',],
     package_dir={'': 'bin',},
-    package_data={'': ['nanogui.cp37-win_amd64.pyd', 'nanogui.dll', 'nanogui.so', 'libnanogui.so'],},
+    package_data={'': ['nanogui.cp37-win_amd64.pyd', 'nanogui.dll', 'nanogui.cpython-36m-x86_64-linux-gnu.so', 'nanogui.cpython-37m-x86_64-linux-gnu.so', 'libnanogui.so'],},
 )
